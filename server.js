9b4dba9b70d684f0ec10d3f9ef48c9ebebbf1456
Hạnh Thân <thanduyhanhlk04@gmail.com>
2023-03-03 21:09:00 +0700

    fix cors

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const dbConnect = require('./config/dbConnect');
-const app = express();
+
 const dotenv = require('dotenv').config();
 const PORT = 5000;
 const authRouter = require('./routes/authRoute');
 const cors = require('cors');
-
+const app = express();
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions))
 dbConnect();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,10 +20,10 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.use(cors())
- app.listen(PORT, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+
+//  app.listen(80, function () {
+//   console.log('CORS-enabled web server listening on port 80')
+// })
 
 app.listen(PORT, () => {
     console.log(`Server is running  at PORT http://localhost:${PORT}`);
